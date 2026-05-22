89271f304ca044c6fb0f170a16949950429cac15
Gerishon Magachi <shongmagachi@gmail.com>
2026-05-22 10:47:24 +0300

    update project configuration

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Search from './components/Search'
+
+
+
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('I am a state')
+  
   return (
-    <h2> Functional Arrow Component </h2>
+    <main>
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span> and TV Shows Easily</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </div>
+      
+    </main>
+    
   )
 }
 
